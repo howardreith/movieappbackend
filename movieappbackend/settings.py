62c0df5e39c580ac155a7ee2ee53c59c00c857b3
howardreith <howard.reith@gmail.com>
2018-11-20 18:29:30 -0500

    Remove inappropriate static URL material
    -Remove inappropriately added static URL material

--- movieappbackend/settings.py
+++ movieappbackend/settings.py
@@ -127,10 +127,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '/static/'
