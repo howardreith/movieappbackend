dec48a35311c2fbb9118170fb9d88d268a5d8460
howardreith <howard.reith@gmail.com>
2018-11-20 19:13:21 -0500

    Add CORS info for heroku
    -Add CORS info for heroku

--- movieappbackend/settings.py
+++ movieappbackend/settings.py
@@ -58,6 +58,11 @@ ROOT_URLCONF = 'movieappbackend.urls'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_METHODS = (
+    'GET',
+    'POST'
+)
+
 # CORS_ORIGIN_WHITELIST = (
 #     'localhost:7165'
 #     'https://howardreith.github.io'
@@ -130,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
